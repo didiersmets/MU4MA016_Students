75354729b6221b797645772135587b56ddbf067f
HugoNaranjo <hugonljr@hotmail.com>
2024-09-22 20:11:08 +0200

    New entry

--- NARANJO/CMake/cmake_install.cmake
+++ NARANJO/CMake/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /root/MU4MA016_Students/NARANJO
+# Install script for directory: /root/MU4MA016_Students/NARANJO/CMake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/root/MU4MA016_Students/NARANJO/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/root/MU4MA016_Students/NARANJO/CMake/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
