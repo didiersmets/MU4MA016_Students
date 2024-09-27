ac38241e62efcc2ff23ce7302bdced456d90b7bf
didiersmets <didier.smets@sorbonne-universite.fr>
2024-09-27 10:32:33 +0200

    TP1: modify circular buffer init prototype

--- TP1/include/circular_buffer_queue.h
+++ TP1/include/circular_buffer_queue.h
@@ -1,19 +1,19 @@
 #include <stdbool.h>
-#include <stddef.h>  // for size_t
+#include <stddef.h> // for size_t
 
 struct Queue {
-	size_t front;	   // index of the first element in the queue
-	size_t length;	   // number of items presently in the queue
-	size_t capacity;   // capacity of the queue (in nbr of items)
-	size_t elem_size;  // length in bytes of each item in the queue
-	void *data;	   // address of the array
+	size_t front; // index of the first element in the queue
+	size_t length; // number of items presently in the queue
+	size_t capacity; // capacity of the queue (in nbr of items)
+	size_t elem_size; // length in bytes of each item in the queue
+	void *data; // address of the array
 };
 
 bool is_empty(const struct Queue *q);
 
 size_t queue_length(const struct Queue *q);
 
-int queue_init(struct Queue *q, size_t elem_size, size_t capacity);
+struct Queue *queue_init(size_t elem_size, size_t capacity);
 
 void queue_dispose(struct Queue *q);
 
