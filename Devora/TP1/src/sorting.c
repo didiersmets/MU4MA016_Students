481885f3fcf4f387a9aa5babf7f8fe46ca5e6f21
adevora <112868917+EarltShirt@users.noreply.github.com>
2024-09-27 12:53:49 +0200

    TP1 - Devora (suite)

--- Devora/TP1/src/sorting.c
+++ Devora/TP1/src/sorting.c
@@ -1,3 +1,6 @@
+#include "sorting.h"
+#include <stdio.h>
+
 void bubble_sort(int * array, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -22,10 +25,46 @@ void insertion_sort(int * array, int n) {
     }
 }
 
-merge(int * T, int p, int q, int r) {
-    
+void merge(int *T, int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = T[p + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = T[q + 1 + j];
+
+    int i = 0, j = 0, k = p;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            T[k] = L[i];
+            i++;
+        } else {
+            T[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        T[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        T[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void merge_sort(int * array, int n) {
-
+void merge_sort(int *T, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        merge_sort(T, left, mid);
+        merge_sort(T, mid + 1, right);
+        merge(T, left, mid, right);
+    }
 }
