8b538e0adb10013c3589a39a9755b770ad6cb897
Meng Sivmeng <mengsivmeng@gmail.com>
2024-10-04 21:11:20 +0200

    [hun] DONE: queue implementation
    - DONE: implemented all methods of Queue
    - TODO: Bonus question
    - TODO: upgrade to C++

--- HUN/tp1/queue/src/circular_buffer_queue.c
+++ HUN/tp1/queue/src/circular_buffer_queue.c
@@ -13,49 +13,25 @@ typedef struct Queue {
   void *data;		// address of the array
 } Queue;
 
-
-
-Queue *queue_init(size_t elem_size , size_t capacity );
+size_t queue_length(const struct Queue *q);
+bool is_empty(const struct Queue *q);
 void queue_enqueue(Queue *q, const void *src);
 void queue_dequeue(Queue *q, void *dest );
-void enlarge_queue_capacity(struct Queue *q);
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-int main() {
-  int x1 = 10;
-  int x2 = 20;
-  int removed;
-
-  Queue *q = queue_init(sizeof(int), 5);
-  
-
-  queue_enqueue(q, &(int){0});
-  queue_enqueue(q, &(int){1});
+void queue_dispose(struct Queue *q);
+static void enlarge_queue_capacity(struct Queue *q);
 
 
+//////////////////////////////////////////////////
+////////////   IMPLEMENTATION   //////////////////
+//////////////////////////////////////////////////
 
-  /* queue_dequeue(q, &removed); */
-
-
-
-  return 0;
-}
-
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 Queue *queue_init(size_t elem_size, size_t capacity) {
   Queue *q = (Queue *)malloc(sizeof(Queue));
-
   q->front = 0;
   q->length = 0;
   q->elem_size = elem_size;
   q->capacity = capacity;
   q->data = malloc(elem_size * capacity);
-
-  //debug
-  
-
   return q;
 }
 
@@ -64,29 +40,24 @@ void queue_enqueue(Queue *q, const void *src) {
     enlarge_queue_capacity(q);
   }
 
-  void *dest =  (char*)q->data + ((q->length+q->front)%q->capacity)*q->elem_size;
+  size_t pos = (q->length+q->front)%q->capacity;
+  void *dest =  (char*)q->data + pos*q->elem_size;
   memcpy(dest, src, q->elem_size);
 
   q->length += 1 ;
-
-  /* // For debuging */
-  /* printf("%d\n", *(int *)dest); */
 }
+
 
 
 void queue_dequeue(Queue *q, void *dest) {
-  if (q->length == 0 ){
-    fprintf(stderr,"Error: Cannot remove elements from empty queue");
-    exit(EXIT_FAILURE);
+  if (q->length > 0) {
+    void *src = (char *)q->data + q->front * q->elem_size;
+    memcpy(dest, src, q->elem_size);
+
+    q->front = (q->front + 1) % q->capacity;
+    q->length--;
   }
-
-  void *src = (char*)q->data + q->front * q->elem_size;
-  memcpy(dest, src, q->elem_size);
-
-  q->front = (q->front+1)%q->capacity;
-  q->length--;
 }
-
 
 void enlarge_queue_capacity(struct Queue *q){
   q->data = realloc(q->data, q->elem_size * q->capacity * 2);
@@ -100,3 +71,12 @@ void enlarge_queue_capacity(struct Queue *q){
   q->capacity *= 2;
 }
 
+void queue_dispose(struct Queue *q) {
+  free(q->data);
+}
+
+
+
+bool is_empty(const struct Queue *q) {
+  return (q->length == 0);
+}
