c885c773b832d9b502b87163997da0133b2062a1
janmarxen <jan.marxen@gmail.com>
2024-10-08 12:53:30 +0200

    Started exercise 1.5

--- JMarxen/TP1/EX1_cpp/src/circular_buffer_queue.c
+++ JMarxen/TP1/EX1_cpp/src/circular_buffer_queue.c
@@ -10,12 +10,12 @@ bool is_empty(const struct Queue *q) { return (q->length) == 0; }
 
 size_t queue_length(const struct Queue *q) { return q->length; }
 
-struct Queue *queue_init(size_t elem_size, size_t capacity) {
+template <typename T>
+struct Queue *queue_init(size_t capacity) {
 	struct Queue *q = malloc(sizeof(struct Queue));
-	q->data = malloc(capacity * elem_size);
+	q->data = (T *)malloc(capacity * sizeof(T));
 	q->front = 0;
 	q->length = 0;
-	q->elem_size = elem_size;
 	q->capacity = capacity;
 	return q;
 }
@@ -26,36 +26,35 @@ void queue_dispose(struct Queue *q) {
 }
 
 // Push the src to the back of the tail
-void queue_enqueue(struct Queue *q, const void *src) {
+template <typename T>
+void queue_enqueue(struct Queue *q, T val) {
 	if (q->length >= q->capacity) {
 		size_t old_capacity = q->capacity;
 		enlarge_queue_capacity(q);
 		// Copy elements before head to after head since capacity was
 		// added.
 		for (int i = 0; i < q->front; i++) {
-			memcpy((char *)(q->data) +
-				   (old_capacity + i) * (q->elem_size),
-			       (char *)(q->data) + i * (q->elem_size),
-			       q->elem_size);
+			q->data[old_capacity + i] = q->data[i];
 		}
 	}
 	// Enqueue the element.
 	size_t tail = (q->front + q->length) % q->capacity;
-	memcpy((char *)(q->data) + tail * (q->elem_size), src, q->elem_size);
+	q->data[tail] = val;
 	q->length++;
 	return;
 }
 
 // Pull the head element to dest
-void queue_dequeue(struct Queue *q, void *dest) {
+template <typename T>
+queue_dequeue(struct Queue *q) {
 	if (is_empty(q)) {
+		return NULL
 	} else {
-		memcpy(dest, (char *)(q->data) + (q->front) * (q->elem_size),
-		       q->elem_size);
+		T return_val = q->data[q->front];
 		q->front = (q->front + 1) % q->capacity;
 		q->length--;
+		return T return_val;
 	}
-	return;
 }
 
 static void enlarge_queue_capacity(struct Queue *q) {
