31bd834364e3f48b56fc2739403ef2fcc47ffd78
HUANG.Kaiwei <hkw1026003132@gmail.com>
2024-10-08 12:45:46 +0200

    8/10

--- HUANG/MA016/TP1/include/circular_buffer_queue.h
+++ HUANG/MA016/TP1/include/circular_buffer_queue.h
@@ -1,70 +1,23 @@
-#include <stdio.h>
 #include <stdbool.h>
 #include <stddef.h> // for size_t
-#include <string.h>
 
-struct Queue
-{
-  size_t front; // index of the first element in the queue
-  size_t length; // number of items presently in the queue
-  size_t capacity; // capacity of the queue (in nbr of items)
-  size_t elem_size; // length in bytes of each item in the queue
-  void *data; // address of the array
+struct Queue {
+	size_t front; // index of the first element in the queue
+	size_t length; // number of items presently in the queue
+	size_t capacity; // capacity of the queue (in nbr of items)
+	size_t elem_size; // length in bytes of each item in the queue
+	void *data; // address of the array
 };
 
-bool is_empty(const struct Queue *q)
-{
-  assert (q != NULL);
-  return (q -> length == 0);
-};
+bool is_empty(const struct Queue *q);
 
-size_t queue_length(const struct Queue *q)
-{
-  assert (q != NULL);
-  return (q->length);
-};
+size_t queue_length(const struct Queue *q);
 
-struct Queue *queue_init(size_t elem_size, size_t capacity)
-{
-  q = malloc(sizeof(Queue));
-  if (q != NULL){
-    q -> front = 0;
-    q -> length = 0;
-    q -> capacity = capacity;
-    q -> elem_size = elem_size;
-    q -> data = NULL;
-  }
-  return (q);
-};
+struct Queue *queue_init(size_t elem_size, size_t capacity);
 
-void queue_dispose(struct Queue *q)
-{
-  if(q == NULL){
-    return;
-  }
-  free(q -> data);
-  free(q);
-};
+void queue_dispose(struct Queue *q);
 
-void queue_enqueue(struct Queue *q, const void *src)
-{
-  if (q -> length == q -> capacity){
-    enlarge_queue_capacit(q);
-  }
-  void * place = (char*)(q->data) + ((q -> front + q -> length)%(q->capacity)) * q->elem_size;
-  memcpy(place,src,q -> elem_size);
-  return (q);
-};
+void queue_enqueue(struct Queue *q, const void *src);
 
-void queue_dequeue(struct Queue *q, void *dest)
-{
-  void * src = (char*)(q->data) + ((q -> front + q -> length)%q->capacity ) * q->elem_size;
-  memcpy(dest , src , q -> elem_size);
-  return(dest);
-};
+void queue_dequeue(struct Queue *q, void *dest);
 
-static void enlarge_queue_capacity ( struct Queue * q )
-{
-  size_t double_cap = 2 * q -> capacity;
-  q -> capacity = double_cap;
-};
