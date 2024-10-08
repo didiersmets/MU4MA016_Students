f7d986972c1bb76c5e7c1e6f71671f81f6a30d43
Yan Lei-Ya <21103458@401-14.math.upmc.fr>
2024-10-08 12:43:13 +0200

    merge sort part 1

---
+++ YAN/src/test_merge.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "merge.h"
+
+int main(){
+	int T[8] = {6,7,11,23,2,3,9,20};
+	int p = 0;
+	int q = 3;
+	int r = 7;
+
+	Merge(T,p,q,r);
+	return 0;
+}
+
+
+
