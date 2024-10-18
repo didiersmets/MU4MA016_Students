20dfa70fe2930daab24ea6fce06ba953911a6560
ItziarRodriguez <itziar.rodriguez.franco@outlook.es>
2024-10-18 12:49:43 +0200

    masdehoy

---
+++ IRF/build/CMakeFiles/tp1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp1.dir/src/circular_buffer_queue.c.o"
+  "CMakeFiles/tp1.dir/src/circular_buffer_queue.c.o.d"
+  "CMakeFiles/tp1.dir/src/test_queue.c.o"
+  "CMakeFiles/tp1.dir/src/test_queue.c.o.d"
+  "tp1"
+  "tp1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
