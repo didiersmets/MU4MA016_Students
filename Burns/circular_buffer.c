425f58190da72e53d1efa0e2c306a3e57ba2c2d5
Burns <thomas.burns@etu.sorbonne-universite.fr>
2024-10-22 11:11:17 +0200

    My first commit

---
+++ Burns/circular_buffer.c
@@ -0,0 +1,43 @@
+#include<stdbool.h>
+#include<stddef.h>
+#include<string.h>
+
+bool_is_empty(const struct Queue *q){
+	return head == -1;
+}
+
+int size_t queue_length(const struct Queue *q){
+	return length;
+}
+
+struct Queue *queue_init(size_t elem_size, size_t capacity){
+	size_t = elem_size;
+	capacity = sum(elem_size);
+}
+
+int void queue_dispose(struct Queue *q){
+	return front;
+}
+
+void queue_enqueue(struct Queue *q, const void *src){
+	if (front == -1){
+
+	front = 0;
+}
+
+length = (length + 1) % capacity;
+fprint("capacity increased by 1");
+
+}
+
+int void queue_dequeue(struct Queue *q, void *dest){
+	if (is_empty()){
+		fprint("Queue is empty");
+		return;
+	}
+}
+
+int static void enlarge_queue_capactiy(struct Queue *q){
+	if (length>capacity)
+	
+}
