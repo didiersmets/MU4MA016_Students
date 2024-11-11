0e22901a12af8288134d2de84a010c25f53f5067
janmarxen <jan.marxen@gmail.com>
2024-11-11 20:09:16 +0100

    Completed TD3.

---
+++ JMarxen/TP2_and_TP3/mesh_project/build/CMakeFiles/tp3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp3.dir/src/hash_tables.c.o"
+  "CMakeFiles/tp3.dir/src/hash_tables.c.o.d"
+  "CMakeFiles/tp3.dir/src/mesh_3D.c.o"
+  "CMakeFiles/tp3.dir/src/mesh_3D.c.o.d"
+  "CMakeFiles/tp3.dir/src/tp3.c.o"
+  "CMakeFiles/tp3.dir/src/tp3.c.o.d"
+  "tp3"
+  "tp3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tp3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
