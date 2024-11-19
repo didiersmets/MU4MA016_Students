fbd13493f001c1bd061ff3a9c5fd18442fbca2c3
HugoNaranjo <hugonljr@hotmail.com>
2024-11-19 01:53:52 +0100

    X

--- NARANJO/TP3/hashtab.c
+++ NARANJO/TP3/hashtab.c
@@ -5,17 +5,19 @@
 #include <string.h>
 
 
-#include "/root/MU4MA016_Students/NARANJO/TP2/tri_mesh.c"
+#include "../MU4MA016_Students/TP3/include/hash_tables.h"
+#include "../MU4MA016_Students/TP3/include/mesh.h"
+
 
 int edge_pos_in_tri ( int v1 , int v2 , struct Triangle t )
 {
-	if (v1 == t.alpha && v2 == t.beta){
+	if (v1 == t.v1 && v2 == t.v2){
 		return 0;
 	}
-	if  (v1 == t.beta && v2 == t.gamma){
+	if  (v1 == t.v2 && v2 == t.v3){
                 return 1;
         }
-	if (v1 == t.gamma && v2 == t.alpha){
+	if (v1 == t.v3 && v2 == t.v1){
                 return 2;
         }
 	else{
@@ -26,9 +28,9 @@ int edge_pos_in_tri ( int v1 , int v2 , struct Triangle t )
 
 int tris_are_neighbors(int tri1, int tri2, const struct Mesh *m)
 {
-	struct Vertex v1 = m->vert[(tri1.alpha)];
-	struct Vertex v2 = m->vert[(tri1.beta)];
-	struct Vertex v3 = m->vert[(tri2.gamma)];
+	struct Vertex v1 = m->vert[(tri1.v1)];
+	struct Vertex v2 = m->vert[(tri1.v2)];
+	struct Vertex v3 = m->vert[(tri2.v3)];
 
 	if (edge_pos_in_tri(v1, v2, tri2) != -1){
 		return edge_pos_in_tri(v1, v2, tri2);
@@ -49,17 +51,17 @@ int tris_are_neighbors(int tri1, int tri2, const struct Mesh *m)
 
 int *build_adjacency_table1(const struct Mesh *m)
 {
-	int adj[3*m->nt];
-	for(int i = 0; i< 3*m->nt, i++){
+	int adj[3*m->ntri];
+	for(int i = 0; i< 3*m->ntri; i++){
 		adj[i] = -1;
 	}
 
-	for (int i = 0; i < m->nt, i++){
-		struct Triangle tri = m->tri[i];
+	for (int i = 0; i < m->ntri; i++){
+		struct Triangle tri = m->triangles[i];
 		for (int j = 0; j < 2; j++){
-			for (int k = 0; k < m->nt, k++){
-				struct Triangle tri2 = m->tri[k];
-				if (i != k && tris_are_neighbors(tri, tri2, m != -1)){
+			for (int k = 0; k < m->ntri; k++){
+				struct Triangle tri2 = m->triangles[k];
+				if (i != k && tris_are_neighbors(tri, tri2, m) != -1){
 					adj[3*i + j] = tris_are_neighbors(tri, tri2, m != -1);
 				}
 			
@@ -68,11 +70,30 @@ int *build_adjacency_table1(const struct Mesh *m)
 		}
 	}
 	return adj;
-		
-
-
 }
 
 
 
+
+int main(){
+
+
+    	struct Mesh2D* mesh = initialize_mesh2D(5, 2);
+
+    	
+    	mesh->vert[0] = (struct Vertex) {0.0, 0.0};
+    	mesh->vert[1] = (struct Vertex) {1.0, 0.0};
+    	mesh->vert[2] = (struct Vertex) {1.0, 1.0};
+    	mesh->vert[3] = (struct Vertex) {0.0, 1.0};
+    	mesh->vert[4] = (struct Vertex) {0.5, 0.5};
+    	mesh->tri[0] = (struct Triangle) {0, 1, 4}; 
+    	mesh->tri[1] = (struct Triangle) {1, 2, 4}; 
+
+    	dispose_mesh2D(mesh);
+
+	int adj = *build_adjacency_table1;
+	printf(adj);
+
+    	return 0;
+}
 
