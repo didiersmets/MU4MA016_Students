3498b4a4d8672065c64bc22616778dfdf4b8de33
emmaeve <emma.eve_perso@orange.fr>
2024-11-22 12:48:34 +0100

    TP2 read mesh and gnuplot

--- EE/TP2/triangular_meshes.c
+++ EE/TP2/triangular_meshes.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <FILE.h>
 
 struct Vertex{
 	union{
@@ -26,8 +27,9 @@ struct Mesh2D{
 	struct Triangle* tri; /*array of triangles*/
 };
 
-int initialise_mesh2D(struct Mesh2D* m, int vtx_capacity, int tri_capacity)
+int initialise_mesh2D(int vtx_capacity, int tri_capacity)
 {
+	struct Mesh2D* m = (struct Mesh2D*)malloc(sizeof(Mesh2D));
 	m->nv = vtx_capacity;
 	m->vert = (struct Vertex*)malloc(vtx_capacity*sizeof(struct Vertex));
 	m->nt = tri_capacity;
@@ -53,3 +55,59 @@ double area_mesh2D(struct Mesh2D* m)
 	return res;
 }
 
+int read_mesh2D(struct Mesh2D *m, const char *file)
+{
+	FILE *fp = fopen(file, "r");
+	if (fp == NULL){
+		printf("impossible de lire le fichier\n");
+		return -1;
+	}
+	char ligne[100];
+	int n;
+	while(!feof(fp)){
+		fgets(ligne, 100, fp);
+		if (sscanf(ligne, "MeshVersionFormatted %d", &n) == 1){
+			if (n != 2)
+				return -1;
+		}
+		if (sscanf(ligne,"Vertices %d", &m->nv) == 1){
+			m->vert = (struct Vertex*)malloc(m->nv*sizeof(struct Vertex));
+			for (int i = 0; i < m->nv; i++){
+				fgets(ligne, 100, fp);
+				sscanf(ligne, "%lf %lf", &m->vert[i].x, &m->vert[i].y);
+			}
+		}
+		if (sscanf(ligne, "Triangles %d", &m->nt) == 1){
+			m->tri = (struct Triangle*)malloc(m->nt*sizeof(struct Vertex));
+			for (int i = 0; i < m->nt; i++){
+				fgets(ligne, 100,fp);
+				sscanf(ligne,"%d %d %d", &m->tri[i].A, &m->tri[i].B, &m->tri[i].C);
+			}
+		}
+	}
+	return 0;
+}
+
+int mesh2D_to_gnuplot(struct Mesh2D* m, const char* filename)
+{
+	FILE *fp = fopen(filename, "w");
+	if (fp == NULL)
+		return -1;
+	for (int i = 0; i < m->nt; i++){
+		fprintf(fp, "%lf", m->vert[m->tri[i].A - 1].x);
+		fprintf(fp, "%lf", m->vert[m->tri[i].A - 1].y);
+		fprintf(fp, "\n");
+		fprintf(fp, "%lf", m->vert[m->tri[i].B - 1].x);
+		fprintf(fp, "%lf", m->vert[m->tri[i].B - 1].y);
+		fprintf(fp, "\n");
+		fprintf(fp, "%lf", m->vert[m->tri[i].C - 1].x);
+		fprintf(fp, "%lf", m->vert[m->tri[i].C - 1].y);
+		fprintf(fp, "\n");
+		fprintf(fp, "%lf", m->vert[m->tri[i].A - 1].x);
+		fprintf(fp, "%lf", m->vert[m->tri[i].A - 1].y);
+		fprintf(fp, "\n");
+		fprintf(fp, "\n");
+		fprintf(fp, "\n");
+	}
+	return 0;
+}
