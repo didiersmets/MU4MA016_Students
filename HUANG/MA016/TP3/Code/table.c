65856a5f0e72c9d19703f4ec8fcd5cbaa117d613
HUANG.Kaiwei <hkw1026003132@gmail.com>
2024-11-29 12:56:23 +0100

    29/11

--- HUANG/MA016/TP3/Code/table.c
+++ HUANG/MA016/TP3/Code/table.c
@@ -3,7 +3,7 @@
 #include "../include/mesh_adjacency.h"
 #include "../src/mesh.c"
 
-int edge_pos_in_tri(int v1,int v2, struct Triangle t)
+int edge_pos_in_tri(int v1,int v2, struct Triangle* t)
 {
 	for(int i=0;i<2;i++){
 		if(v1 == t[i%3] && v2 == t[(i+1)%3]){
@@ -30,7 +30,21 @@ int tris_are_neighbors(int tri1,int tri2,const struct Mesh *m)
 
 int *build_adjacency_table1(const struct Mesh *m)
 {
-	int adj[3*m->nt];
-
+	int* adj=malloc(3*m->nt*sizeof(int));
+	for(int i=0;i<3*m->nt;i++){
+		adj[i]=-1;
+	}
+	for(int i=0;i<m->nt;i++){
+		for(int j=i+1;j<m->nt;j++){
+			com_pos = tris_are_neighbors(i,j,m);
+			if (com_pos!=-1){
+				adj[3*i+com_pos] = j;
+			}
+		}
+	}
 	return adj;
+}
+
+struct HashTable *build_edge_table1(const struct Mesh *m){
+
 }
