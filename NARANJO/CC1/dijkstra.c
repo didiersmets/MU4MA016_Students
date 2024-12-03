1a200994b6db663ce0aa206ab6706c7f526f0181
HugoNaranjo <hugonljr@hotmail.com>
2024-12-04 00:10:30 +0100

    CC1 :(

--- NARANJO/CC1/dijkstra.c
+++ NARANJO/CC1/dijkstra.c
@@ -1,4 +1,10 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include <limits.h>
 
 #include "dijkstra.h"
 #include "graph.h"
@@ -12,13 +18,28 @@
 struct DijkstraHeap {
 	size_t size;
 	size_t capacity;
-	HEdge *data;
+	struct HEdge* data;
 };
+
+DijkstraHeap* createHeap(int capacity)
+{
+    struct DijkstraHeap* heap = (struct DijkstraHeap*)malloc(sizeof(DijkstraHeap));
+    heap->size = 0;
+    heap->capacity = capacity;
+    heap->array = (struct HEdge**)malloc(capacity * sizeof(HEdge*));
+    return heap;
+}
+
+
 
 /* You should implement these two ONLY if you have already implemented
  * Dijkstra_solve. */
 static void heap_push(struct DijkstraHeap *heap, struct HEdge edge);
-static struct HEdge heap_pop(struct DijkstraHeap *heap);
+
+
+static struct HEdge heap_pop(struct DijkstraHeap *heap)
+
+
 /* Implement them here. Worth 5 points in total */
 
 /* 1) Initialize a DikstraSol and and DijkstraHeap.
@@ -36,9 +57,33 @@ static struct HEdge heap_pop(struct DijkstraHeap *heap);
  *    Each time some half edge is pushed into heap, DijkstraSol should be
  *    updated accordingly.
  */
+
+
 struct DijkstraSol *Dijkstra_solve(struct Graph *G, int source);
 /* Implement me here. Worth 5 points */
+{
+	struct DijkstraHeap* heap = createHeap(graph->nedges);
+	struct DijkstraSol* dj = (DijkstraSol*)malloc(sizeof(DijkstraSol));
+    	dj->pred = (int*)malloc(graph->nverts * sizeof(int));
+   	dj->cost = (int*)malloc(graph->nverts * sizeof(int));
+	
+	for(int i = 0; i<graph->nverts; i++){
+		dj->pred[i] = -1;
+	}
 
+	dj-pred[source] = source;
+	dj-cost[source] = 0;
+	struct HEdge edge;
+	edge->cost = 0;
+	edge->dest = source;
+	
+	heap_push(heap, edge);
+	while(!IsEmpty(heap)){
+		e = heap_pop(heap);
+	// NOT DONE... SAD FACE :(
+	}
+    	
+}
 /* Follow predecessors back from dest until source and (pretty) print them */
 void print_minimal_path(int source, int dest, struct DijkstraSol *sol);
 /* Implement me here. Worth 2 points */
