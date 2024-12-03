451797c9c6e108173bcfdcf01ba431ec96312a2f
ItziarRodriguez <itziar.rodriguez.franco@outlook.es>
2024-12-03 12:47:57 +0100

    3dic

--- IRF/TP3/hashtable_mesh.c
+++ IRF/TP3/hashtable_mesh.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "hash_table.h"
-#include "mesh.h"
+#include "include/mesh_adjacency.h"
+#include "include/mesh.h"
 
 int edge_pos_in_tri(int v1, int v2, struct Triangle t)
 {
+	if ((t.idx[0] == v1) && (t.idx[1] == v2)){
+		return 0;
+	}
+	if ((t.idx[1] == v1) && (t.idx[2] == v2)){
+		return 1;
+	}
+	if ((t.idx[2] == v1) && (t.idx[0] == v2)){
+		return 2;
+	}
+	else {
+		return -1;
+	}
+}
+
+int tris_are_neighbors(int tri1, int tri2, const struct Mesh *m)
+{
+	int *idx = 
+	for (int i = 0; i<3; i++){
+		int *idx1 = m->triangles[tri1].idx[(i+1)%3];
+		int *idx2 = m->triangles[tri1].idx[i]
+		if (edge_pos_in_tri(idx1, idx2, m->triangles[tri2]) != -1){
+			return edge_pos_in_tri(idx2, idx1, m->triangles[tri1]);
+		}	
+	}
+	return -1;
+}
+
+		
+
+
+
+
+
+
+
+
 
 
