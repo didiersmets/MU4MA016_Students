7791321efafd19c0672374bd2e9337f1e4b5ba05
AudreyDrieux <audrey.drieux@gmail.com>
2024-12-03 12:50:35 +0100

    TP3

--- ADrieux/TP3/hashtable.c
+++ ADrieux/TP3/hashtable.c
@@ -3,6 +3,7 @@
 
 #include "/users/home/28624202/MU4MA016_Students/TP3/include/mesh_adjacency.h"
 #include "/users/home/28624202/MU4MA016_Students/TP3/include/mesh.h"
+#include "/users/home/28624202/MU4MA016_Students/TP3/include/hash_tables.h"
 
 int edge_pos_in_tri(int v1, int v2, struct Triangle t)
 {
@@ -22,6 +23,49 @@ int edge_pos_in_tri(int v1, int v2, struct Triangle t)
 		return -1;
 	}
 }
+
+int tris_are_neighbors(int tri1, int tri2, const struct Mesh *m)
+{
+	if (edge_pos_in_tri(m->triangles[tri1].idx[1], m->triangles[tri1].idx[0], m->triangles[tri2]) != -1)
+	{
+		return(edge_pos_in_tri(m->triangles[tri1].idx[0], m->triangles[tri1].idx[1], m->triangles[tri1]));
+	}
+	if (edge_pos_in_tri(m->triangles[tri1].idx[2], m->triangles[tri1].idx[1], m->triangles[tri2]) != -1)
+	{
+		return(edge_pos_in_tri(m->triangles[tri1].idx[1], m->triangles[tri1].idx[2], m->triangles[tri1]));
+	}
+	if (edge_pos_in_tri(m->triangles[tri1].idx[0], m->triangles[tri1].idx[2], m->triangles[tri2]) != -1)
+	{
+		return(edge_pos_in_tri(m->triangles[tri1].idx[2], m->triangles[tri1].idx[0], m->triangles[tri1]));
+	}
+	else {
+		return -1;
+	}
+}
+
+int *build_adjacency_table1(const struct Mesh *m)
+{
+	int *adj = (int *)malloc(3*m->ntri*sizeof(int));
+	for (int i = 0; i<m->ntri; i++)
+	{
+		for (int k = 0; k<m->ntri; k++)
+		{
+			if (tris_are_neighbors(i, k, m) != -1)
+			{
+				adj[3*i + tris_are_neighbors(i, k, m)] = k;
+			} else {
+				adj[3*i + tris_are_neighbors(i, k, m)] = -1;
+			}
+		}
+	}
+	return adj;
+}
+
+struct HashTable *build_edge_table1(const struct Mesh *m)
+{
+	struct HashTable *ht = hash_table_init(
+
+
 
 
 
