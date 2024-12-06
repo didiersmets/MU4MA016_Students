1a8a9570547bd8fde3ed13147d286b13cc9c288c
didiersmets <didier.smets@sorbonne-universite.fr>
2024-12-06 10:45:02 +0100

    upload CC1

---
+++ FARAT/CC1/graph.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "graph.h"
+
+struct EdgeList *read_edge_list_from_file(const char *filename);
+/* Implement me here. Worth 5 points */
+
+struct Graph *build_graph_from_edge_list(const EdgeList *EL);
+/* Implement me here. Worth 5 points */
