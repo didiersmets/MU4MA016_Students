6b878ce99894ac05248b63cd5741801930abb1f3
Costoso <Edgar.Costoso@etu.sorbonne-universite.fr>
2025-09-09 11:23:45 +0200

    The first commit of Costoso

---
+++ Students/Costoso/build/CMakeFiles/read_file.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_file.dir/read_file.cpp.o"
+  "CMakeFiles/read_file.dir/read_file.cpp.o.d"
+  "read_file"
+  "read_file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Students/Costoso/hello.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+/*
+Construire cmake liste
+1) Faire CMakeLists.txt dans dir
+2) Ecrire:
+    cmake_minimum_required(VERSION 3.14)
+    project(tp0)
+
+    add_executable(hello
+	    hello.cpp
+    )
+3) Faire dans terminal:
+    cmake -B build/
+    cmake --build build
+4) Executer avec:
+    ./build/hello
+*/
+int Pascal_formula(int i,int j){ 
+    if( j>0 && j<i ){
+        return Pascal_formula(i-1,j-1)+Pascal_formula(i-1,j);
+    }
+    else{
+        return 1;
+    }
+}
+
+int main(){
+    std::cout << "Hello World\n";
+    std::cout << "Triangle de Pascal\n";
+    int n=5;
+    for(int i=0 ; i<=n ; i++){
+        for(int j=0 ; j<=i ; j++){
+            std::cout << Pascal_formula(i,j) <<" ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+
