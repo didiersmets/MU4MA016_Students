d6e8aa179438f06f156f20e8566279f373235947
pascalkessler <passypost@gmail.com>
2025-09-11 17:32:11 +0200

    write impl and gnuplot

---
+++ Students/KESSLER/tp0/ReadandWrite/CMakeFiles/write_file.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/pascalkessler/Studium/PARIS/Data Structures/MU4MA016_Students/Students/KESSLER/tp0/ReadandWrite/write.c" "CMakeFiles/write_file.dir/write.c.o" "gcc" "CMakeFiles/write_file.dir/write.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Students/KESSLER/tp0/ReadandWrite/CMakeFiles/write_file.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/write_file.dir/write.c.o"
+  "CMakeFiles/write_file.dir/write.c.o.d"
+  "write_file"
+  "write_file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/write_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
