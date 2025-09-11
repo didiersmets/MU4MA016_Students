37866d686e87f2ae02d016f35b1c9729ecbae90e
MathisPoppe <mathispoppe1@gmail.com>
2025-09-11 17:59:51 +0200

    added bubble and insertion sort

---
+++ Students/Poppe/TP1/src/bubble_sort.cpp
@@ -0,0 +1,13 @@
+#include <utility>
+
+#include "bubble_sort.hpp"
+
+void bubble_sort(int* array, size_t size) {
+    for (size_t i = 0; i < size - 1; ++i) {
+        for (size_t j = 0; j < size - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                std::swap(array[j], array[j + 1]);
+            }
+        }
+    }
+}
