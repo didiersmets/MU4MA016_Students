1a5c7f25fdb9b6bd69c8d09423acb0d6b376b025
sidate <sidate.mathieu@outlook.fr>
2025-09-11 17:47:37 +0200

    commit of sidate

---
+++ Students/sidate/tp1/src/circular_buffer_queue.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <circular_buffer_queue.h>
+
+static void enlarge_queue_capacity(struct Queue *q);
+
+
+void enlarge_queue_capacity(struct Queue *q){
+	if(size_t lenght  >= size_t capacity){
+	}
+}
+
+void queue_enqueue(struct Queue *q, const void *src){
+	memcpy(src, data, elem_size * q.lenght);
+	
+
+
+
