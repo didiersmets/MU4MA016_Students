e567bcecde7fc8e0187cb50dbe9945468d2aeb9e
sidate <sidatemathieu@gmail.com>
2025-09-22 19:37:47 +0200

    not done yet 22.09

--- Students/sidate/tp1/src/circular_buffer_queue.c
+++ Students/sidate/tp1/src/circular_buffer_queue.c
@@ -7,23 +7,35 @@ static void enlarge_queue_capacity(struct Queue *q);
 
 
 void enlarge_queue_capacity(struct Queue *q){
-	if(q.length  >= q.capacity){
-		size_t l = q.elem_size * q.length + 1;
-		q.data = (void *) malloc(2*(l * q.elem_size));
-
+	if(q->length  >= q->capacity){
+		size_t l = q->elem_size * q->length + 1;
+		q->data = (void *) malloc(2*(l * q->elem_size));
 	}
+  free(q->data);
+  q->data = malloc(q->elem_size * q->capacity);
 }
 
 void queue_enqueue(struct Queue *q, const void *src){
-	const void *src = (const char *)q->data + ((q->front + q->length)%q->capacity)*q-> elem_size;
-	memecpy(q.data, src, q.elem_size * q.length);
+  if(q->length  >= q->capacity){
+    enlarge_queue_capacity(q);
+  }
+  const void *src = (const char *)q->data + ((q->front + q->length)%q->capacity)*q-> elem_size;
+	memcpy(q->data, src, q->elem_size * q->length);
 }
 
 void queue_dequeue(struct Queue *q, void *dest){
+  const void *src = (const char*)q->data + (q->front * q->elem_size);
+  memcpy(dest, src, q->elem_size);
+  q->front += 1;
+  q->lenght -= 1;
+}
 
-	memcpy(dest, q.data, q.elem_size * q.length);
-
-	
-
-
+struct Queue *queue_init(size_t elem_size, size_t capacity){
+    struct Queue *q = malloc(sizeof(struct Queue));
+    q->front=0;
+    q->lenght=0;
+    q->capacity=capacity;
+    q->elem_size=elem_size;
+    return q;
+  }	
 
