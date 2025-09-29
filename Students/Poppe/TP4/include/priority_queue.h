4072d913603e491d5d5ae2da8dd4fb7817039de2
MathisPoppe <mathispoppe1@gmail.com>
2025-09-29 14:58:50 +0200

    added priority queue

---
+++ Students/Poppe/TP4/include/priority_queue.h
@@ -0,0 +1,16 @@
+struct priority_data {
+    int id;
+    float val;
+};
+
+struct priority_queue {
+    struct priority_data *heap;
+    int *pos_in_heap;
+    int size;
+};
+
+int priority_queue_init(struct priority_queue *q, int max_id);
+void priority_queue_free(struct priority_queue *q);
+void priority_queue_push(struct priority_queue *q, int id, float val);
+struct priority_data priority_queue_pop(struct priority_queue *q);
+void priority_queue_update(struct priority_queue *q, int key , float new_val);
