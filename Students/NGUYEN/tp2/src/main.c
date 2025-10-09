e663382e059f8e7bf19fbd96e183f75ae82ef5fa
ThomasNguyen <tstlnguyen@gmail.com>
2025-10-09 17:52:57 +0200

    TP 2 trying to finish read_mesh2D

--- Students/NGUYEN/tp2/src/main.c
+++ Students/NGUYEN/tp2/src/main.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 int main() {
-    struct Mesh2D m;
-    read_mesh2D(&m, "../mesh1-tp2_2D.mesh");
-    mesh2D_to_gnuplot(&m, "mesh.dat");
-    dispose_mesh2D(&m);
+    struct Mesh2D *m;
+    //initialize_mesh2D(m, 10, 10);
+    read_mesh2D(m, "../mesh1-tp2_2D.mesh");
+    mesh2D_to_gnuplot(m, "mesh.dat");
+    //dispose_mesh2D(m);
     printf("GNUplot data file 'mesh.dat' generated successfully.\n");
     return 0;
 }
