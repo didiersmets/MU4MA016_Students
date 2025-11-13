4cadf68b0f719e11bd7b3e3b74fd766b3b3a7053
Costoso <Edgar.Costoso@etu.sorbonne-universite.fr>
2025-11-13 17:57:33 +0100

    Project v2

--- Students/Costoso/Project/Project.h
+++ Students/Costoso/Project/Project.h
@@ -2,21 +2,22 @@
 
 
 
-struct K3_node{
+struct K3_node {
     int x;
     int y;
     int z;
-}
+};
 
-class K3_tree{
-    private:
-        class K3_tree* Left_child;
-        class K3_tree* Right_child;
-        struct K3_node node;
-        int Height;
-        int Hyperplan;
-    public:
-        K3_tree(int Height_, struct K3_node node_, int Hyperplan_): Height(Height_), node(node_), Hyperplan(Hyperplan_){}
-        K3_tree(struct K3_node* Points, int size, int Hyperplan_);
-        void push_back(struct K3_node new_node, K3_tree* tree);
+class K3_tree {
+private:
+    class K3_tree* Left_Child;
+    class K3_tree* Right_Child;
+    class K3_tree* Pred;
+    struct K3_node node;
+    int Hyperplan;
+public:
+    K3_tree(int Height_, struct K3_node node_, int Hyperplan_) : node(node_), Hyperplan(Hyperplan_) {}
+    K3_tree(struct K3_node* Points, int size, int Hyperplan_, K3_tree* Pred_);
+    void push_back(struct K3_node new_node, K3_tree* tree);
+    struct K3_node* Search_p_neighbours(struct K3_node, int p);
 };
