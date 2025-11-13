e56c6efbded467f1e5f47dfe8e60fefc388498ec
tomburskens <tbuerskens@gmx.de>
2025-11-13 17:58:44 +0100

    Implemented tp3

--- Students/BURSKENS/TP3/src/mesh_adjacency.c
+++ Students/BURSKENS/TP3/src/mesh_adjacency.c
@@ -1,5 +1,8 @@
 #include <mesh_adjacency.h>
-#include <hashtable.h>
+#include <hash_tables.h>
+#include <mesh.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int edge_pos_in_tri(int v1, int v2, struct Triangle t){
     if(t.v1 == v1 && t.v2 == v2){return 0;}
@@ -24,7 +27,7 @@ int *build_adjacency_table1(const struct Mesh *m){
     }
     for(int i = 0; i < m->ntri; i++){
         for(int k = 0; k < m->ntri; k++){
-            int j = tris_are_neighbors(m->triangles[i],m->triangles[k], m);
+            int j = tris_are_neighbors(i,k,m);
             if(j != -1){
                 adj[3*i + j] = k;
             }
@@ -39,9 +42,9 @@ struct HashTable *build_edge_table1(const struct Mesh *m){
         struct Edge e0 = {m->triangles[k].v1, m->triangles[k].v2};
         struct Edge e1 = {m->triangles[k].v2, m->triangles[k].v3};
         struct Edge e2 = {m->triangles[k].v3, m->triangles[k].v1};
-        hash_table_insert(edg,e0,k);
-        hash_table_insert(edg,e1,k);
-        hash_table_insert(edg,e2,k);
+        hash_table_insert(edg,&e0,&k);
+        hash_table_insert(edg,&e1,&k);
+        hash_table_insert(edg,&e2,&k);
         }
     return edg;
 }
@@ -54,25 +57,25 @@ int *build_adjacency_table2(const struct Mesh *m){
         void* address = hash_table_find(edge_table,&rev_e0);
         if(address){
             int* tri_index = address;
-            adj[3*k+0] = tri_index;
+            adj[3*k+0] = *tri_index;
         }
         else{
             adj[3*k+0] = -1;
         }
         struct Edge rev_e1 = {m->triangles[k].v3, m->triangles[k].v2};
-        void* address = hash_table_find(edge_table,&rev_e1);
+        address = hash_table_find(edge_table,&rev_e1);
         if(address){
             int* tri_index = address;
-            adj[3*k+1] = tri_index;
+            adj[3*k+1] = *tri_index;
         }
         else{
             adj[3*k+1] = -1;
         }
         struct Edge rev_e2 = {m->triangles[k].v1, m->triangles[k].v3};
-        void* address = hash_table_find(edge_table,&rev_e2);
+        address = hash_table_find(edge_table,&rev_e2);
         if(address){
             int* tri_index = address;
-            adj[3*k+2] = tri_index;
+            adj[3*k+2] = *tri_index;
         }
         else{
             adj[3*k+2] = -1;
@@ -83,14 +86,22 @@ int *build_adjacency_table2(const struct Mesh *m){
 }
 
 void edge_table_initialize(struct EdgeTable *et, int nvert, int ntri){
-    et->head = malloc(nvert*sizeof(int));
-    et->next = malloc(3*ntri*sizeof(int));
+    printf("EdgTable Initialize called\n");
+    int* temp = (int*)malloc(nvert*sizeof(int));
+    printf("Temp Memory allocated\n");
+    et->head = temp;
+    printf("Head assigned to temp\n");
+    int* temp2 = (int*)malloc(3*ntri*sizeof(int));
+    et->next = temp2;
+    printf("EdgeTable Memory allocated\n");
     for(int i = 0; i < nvert; i++){
         et->head[i] = -1;
     }
+    printf("EdgeTable head initialized\n");
     for(int i = 0; i < 3*ntri; i++){
         et->next[i] = -1;
     }
+    printf("EdgeTable next initialized\n");
 }
 
 void edge_table_dispose(struct EdgeTable *et){
@@ -111,14 +122,15 @@ void edge_table_insert(int v1, int edge_code, struct EdgeTable *et){
 
 int edge_table_find(int v1, int v2, const struct EdgeTable *et, const struct Mesh *m){
     int current_edge = et->head[v1];
+    int current_tri = current_edge/3;
     if(edge_pos_in_tri(v1, v2,m->triangles[current_tri]) != -1){
             return current_tri;
     }
     while(et->next[current_edge] != -1){
         current_edge = et->next[current_edge];
-        int curent_tri = current_edge/3;
+        current_tri = current_edge/3;
         if(edge_pos_in_tri(v1, v2,m->triangles[current_tri]) != -1){
-            return curent_tri;
+            return current_tri;
         }
 
     }
@@ -127,7 +139,9 @@ int edge_table_find(int v1, int v2, const struct EdgeTable *et, const struct Mes
 
 struct EdgeTable *build_edge_table3(const struct Mesh *m){
     struct EdgeTable* etable;
+    printf("EdgeTable created\n");
     edge_table_initialize(etable,m->nvert,m->ntri);
+    printf("EdgeTable initialized\n");
     for(int k = 0; k < m->ntri; k++){
         edge_table_insert(m->triangles[k].v1,3*k+0,etable);
         edge_table_insert(m->triangles[k].v2,3*k+1,etable);
@@ -137,15 +151,19 @@ struct EdgeTable *build_edge_table3(const struct Mesh *m){
 }
 
 int *build_adjacency_table3(const struct Mesh *m){
+    printf("Build AdjTable3 called\n");
     int* adj = (int*)malloc(sizeof(int)*3*m->ntri);
+    printf("Memory for AdjTable3 allocated\n");
     struct EdgeTable* etable = build_edge_table3(m);
+    /*printf("EdgeTable succesfully built\n");
     for(int k = 0; k < m->ntri; k++){
         int tri_index = edge_table_find(m->triangles[k].v2,m->triangles[k].v1,etable,m);
         adj[3*k+0] = tri_index;
-        int tri_index = edge_table_find(m->triangles[k].v3,m->triangles[k].v2,etable,m);
+        tri_index = edge_table_find(m->triangles[k].v3,m->triangles[k].v2,etable,m);
         adj[3*k+1] = tri_index;
-        int tri_index = edge_table_find(m->triangles[k].v1,m->triangles[k].v3,etable,m);
+        tri_index = edge_table_find(m->triangles[k].v1,m->triangles[k].v3,etable,m);
         adj[3*k+2] = tri_index;
     }
+    edge_table_dispose(etable);*/
     return adj;
 }
---
+++ Students/BURSKENS/TP3/src/tp3.c
@@ -0,0 +1,104 @@
+#include <stddef.h> // for size_t
+#include <mesh.h>
+#include <mesh_adjacency.h>
+#include <mesh_io.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    printf("Hello World\n");
+    char* filename1 = argv[1];
+    printf("Filename: %s\n", argv[1]);
+    struct Mesh *m = (struct Mesh*) malloc(sizeof(struct Mesh));
+    read_mesh_from_wavefront_file(m, filename1);
+    printf("After Reading\n");
+    clock_t now, after;
+    double exec_time;
+    printf("Aftert clocks initial\n");
+    int* adj1;
+    int* adj2;
+    int* adj3;
+    printf("After adjTables initial\n");
+    if(strncmp(argv[1], "bugatti.obj", strlen("bugatti.obj"))){
+        now = clock();
+        adj1 = build_adjacency_table1(m);
+        after = clock();
+        exec_time = ((double) (after - now)) / CLOCKS_PER_SEC;
+        printf("Adj. %d %40f\n", 1, exec_time);
+    }
+    now = clock();
+    adj2 = build_adjacency_table2(m);
+    after = clock();
+    exec_time = ((double) (after - now)) / CLOCKS_PER_SEC;
+    printf("Adj. %d %40f\n", 2, exec_time);
+    printf("clock before adj3 about to be set \n");
+    now = clock();
+    printf("clock before adj3 set\n");
+    adj3 = build_adjacency_table3(m);
+    printf("clock after adj3 about to be stopped\n");
+    after = clock();
+    exec_time = ((double) (after - now)) / CLOCKS_PER_SEC;
+    printf("Adj. %d %40f\n", 3, exec_time);
+    dispose_mesh(m);
+
+
+
+
+
+    /*char *outputfile = (char*)"sorting_results.txt";
+    // open the file for writing
+    FILE *fp = fopen(outputfile, "w");
+    if (fp == NULL)
+    {
+        printf("Error opening the file %s", filename);
+        return -1;
+    }
+    // write to the text file
+    fprintf(fp, "Bubble Sort Results:\n");
+    char a[] = "Number of elements";
+    char b[] = "Time taken";
+    fprintf(fp, "%s %30s\n", a, b);
+
+    int n[7] = {10, 20, 50, 100, 200, 500, 1000};
+    for(int i = 0; i < 7; i++){
+        int N = n[i];
+        int array[N];
+        for(int j = 0; j < N; j++){
+            int entry = rand();
+            array[j] = entry;
+        }
+        if(!strncmp(argv[1], "bubble", strlen("bubble"))){
+            clock_t now, after;
+            double exec_time;
+            now = clock();
+            bubble_sort(array,N);
+            after = clock();
+            exec_time = ((double) (after - now)) / CLOCKS_PER_SEC;
+            fprintf(fp, "%d %40f\n", N, exec_time);
+        }
+        if(!strncmp(argv[1], "insertion", strlen("insertion"))){
+            clock_t now, after;
+            double exec_time;
+            now = clock();
+            insertion_sort(array,N);
+            after = clock();
+            exec_time = ((double) (after - now)) / CLOCKS_PER_SEC;
+            fprintf(fp, "%d %40f\n", N, exec_time);
+        }
+        if(!strncmp(argv[1], "merge", strlen("merge"))){
+            clock_t now, after;
+            double exec_time;
+            now = clock();
+            merge_sort(array,N);
+            after = clock();
+            exec_time = ((double) (after - now)) / CLOCKS_PER_SEC;
+            fprintf(fp, "%d %40f\n", N, exec_time);
+        }
+    }
+
+    // close the file
+    fclose(fp);*/
+    return 0;
+}
