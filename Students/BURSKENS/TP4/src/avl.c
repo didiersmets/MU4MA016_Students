d6c9d6ba35f7a5c6ff95783ff9f26861f4c73e7c
tomburskens <tbuerskens@gmx.de>
2025-11-24 15:15:32 +0100

    avl functions cleaned up

--- Students/BURSKENS/TP4/src/avl.c
+++ Students/BURSKENS/TP4/src/avl.c
@@ -20,7 +20,7 @@ static struct avl_node* avl_rotation_right(struct avl_node* pivot){
     new_root->right = pivot;
     update_height(pivot);
     update_height(new_root);
-    return new_root;    
+    return new_root;
 }
 
 static struct avl_node* avl_rotation_left(struct avl_node* pivot){
@@ -29,7 +29,7 @@ static struct avl_node* avl_rotation_left(struct avl_node* pivot){
     new_root->left = pivot;
     update_height(pivot);
     update_height(new_root);
-    return new_root;    
+    return new_root;
 }
 
 void *avl_tree_find(struct avl_tree *t, const void *data){
@@ -50,63 +50,74 @@ void *avl_tree_find(struct avl_tree *t, const void *data){
     return NULL;
 }
 
-void avl_tree_insert(struct avl_tree *t, void *data){
-    struct avl_node** current = &t->root;
-    struct avl_node* parent = NULL;
-    while(*current != NULL){
-        void* current_data = (void*)((char*)(*current) - t->offset);
-        int cmp_result = t->cmp(data, current_data);
-        parent = *current;
-        if(cmp_result < 0){
-            current = &((*current)->left);
-        }
-        else{
-            current = &((*current)->right);
-        }
-    }
-    struct avl_node* new_node = malloc(sizeof(struct avl_node));
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->height = 1;
-    *current = new_node;
-
-    // Rebalance the tree
-    current = &t->root;
+void rebalance_tree_post_insert(struct avl_tree *t, const void *data){
+    struct avl_node* current = t->root;
     while(current != NULL){
-        update_height(*current);
-        int balance_factor = ((*current)->left ? (*current)->left->height : 0) - 
-                             ((*current)->right ? (*current)->right->height : 0);
+        update_height(current);
+        int balance_factor = (current->left ? current->left->height : 0) -
+                             (current->right ? current->right->height : 0);
         if(balance_factor > 1){
-            // Left heavy
-            if(t->cmp(data, (void*)((char*)(*current)->left - t->offset)) < 0){
-                // Left-Left case
-                *current = avl_rotation_right(*current);
+            if(t->cmp(data, (void*)((char*)current->left - t->offset)) < 0){
+                current = avl_rotation_right(current);
             }
             else{
-                // Left-Right case
-                (*current)->left = avl_rotation_left((*current)->left);
-                *current = avl_rotation_right(*current);
+                current->left = avl_rotation_left(current->left);
+                current = avl_rotation_right(current);
             }
         }
         else if(balance_factor < -1){
-            // Right heavy
-            if(t->cmp(data, (void*)((char*)(*current)->right - t->offset)) > 0){
-                // Right-Right case
-                *current = avl_rotation_left(*current);
+            if(t->cmp(data, (void*)((char*)current->right - t->offset)) > 0){
+                current = avl_rotation_left(current);
             }
             else{
-                // Right-Left case
-                (*current)->right = avl_rotation_right((*current)->right);
-                *current = avl_rotation_left(*current);
+                current->right = avl_rotation_right(current->right);
+                current = avl_rotation_left(current);
             }
         }
-        if(parent == NULL) break;
-        if(t->cmp(data, (void*)((char*)parent - t->offset)) < 0){
-            current = &(parent->left);
+        if(t->cmp(data, (void*)((char*)current - t->offset)) < 0){
+            current = (current->left);
         }
         else{
-            current = &(parent->right);
+            current = (current->right);
         }
-        parent = NULL; // To avoid infinite loop, as we don't keep track of ancestors here
     }
+}
+
+void avl_tree_insert(struct avl_tree *t, void *data){
+    struct avl_node* current = t->root;
+    struct avl_node* parent = NULL;
+    struct avl_node* visited_nodes = malloc(sizeof(struct avl_node)*t->root->height);
+    for(int i = 0; i < t->root->height; i++){
+        visited_nodes[i] = NULL;
+    }
+    int index = 0;
+    while(current != NULL){
+        void* current_data = (void*)((char*)(current) - t->offset);
+        int cmp_result = t->cmp(data, current_data);
+        visited_nodes[i] = current;
+        i++;
+        parent = current;
+        if(cmp_result < 0){
+            current = (current->left);
+        }
+        else{
+            current = (current->right);
+        }
+    }
+    if(current == NULL){
+        void* parent_data = (void*)((char*)(parent) - t->offset);
+        int cmp_result = t->cmp(data, parent_data);
+        (void*)((char*)(data) + t->offset)->height = 1;
+        if(cmp_result < 0){
+            parent->left = (void*)((char*)(data) + t->offset);
+        }
+        else{
+            parent->right = (void*)((char*)(data) + t->offset);
+        }
+    }
+    int end = t->root->height;
+    for(int i = 1; i <= end; i++){
+        update_height(visited_nodes[end-1]);
+    }
+    rebalance_tree_post_insert(t,data);
 }
