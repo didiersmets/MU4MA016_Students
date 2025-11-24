1d5278c15f81700dca1f8c8b749c07090cf13ea0
Luce Florian <21114147@401-16.math.upmc.fr>
2025-11-24 15:25:36 +0100

    a

--- Students/Luce/TP2/Exo1.c
+++ Students/Luce/TP2/Exo1.c
@@ -9,10 +9,9 @@ typedef struct Vertex{
 }Vertex;
 
 typedef struct Triangle{
-    int index;
-    Vertex pt1;
-    Vertex pt2;
-    Vertex pt3;
+    int index_pt1;
+    int index_pt2;
+    int index_pt3;
 }Triangle;
 
 typedef struct Mesh2D{
@@ -30,19 +29,42 @@ int initialize_mesh2D(struct Mesh2D* m, int vtx_capacity, int tri_capacity){
     m->vert = malloc(sizeof(Vertex)*m->nv);
     m->tri = malloc(sizeof(Triangle)*m->nt);
 
-    /*srand(time(NULL));
-    for(int i=0; i<(m->nv); i++){
-        m->vert[i].x = (double) (rand()%(m->nv+1)); //comme il n'est pas spécifié quels points on doit mettre dans le tableau
-        m->vert[i].y = (double) (rand()%(m->nv+1));
-    }*/
-
-
-
-
+    if (!m->vert || !m->tri){
+        return 0;
+    }
     return 1;
-
 }
 
+void dispose_mesh2D(struct Mesh2D*m){
+    if (!m) return;
+    free(m->vert);
+    free(m->tri);
+    free(m);
+}
+
+double area_mesh2D(struct Mesh2D* m){ //je comprends que c'est la somme de l'air de chaque triangle de mesh2D
+
+    double aire = 0;
+
+    for(int i=0; i<(m->nt); i++){
+
+        Vertex A = m->vert[(m->tri[i]).index_pt1]; //points A,B et C du trangle i
+        Vertex B = m->vert[(m->tri[i]).index_pt2];
+        Vertex C = m->vert[(m->tri[i]).index_pt3];
+
+        Vertex AB; //vecteur AB
+        AB.x = B.x - A.x;
+        AB.y = B.y - A.y;
+
+        Vertex AC;
+        AC.x = C.x - A.x;
+        AC.y = C.y - A.y;
+
+        aire += 1/2*(AB.x*AC.y-AC.x*AB.y); //déterminant de la matrice composée des vecteurs colonnes AB et AC, qui divisé par 2 donne l'aire signé du trangle ABC
+    }
+
+    return aire;
+}
 
 int main(){
     return 0;
