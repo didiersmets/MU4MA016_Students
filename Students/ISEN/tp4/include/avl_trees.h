1266723f62deb43d9e291b30413d53b73edd9040
ISEN <carla.isen@web.de>
2026-01-09 23:17:03 +0100

    TP4 exo 2, test missing

--- Students/ISEN/tp4/include/avl_trees.h
+++ Students/ISEN/tp4/include/avl_trees.h
@@ -14,4 +14,7 @@ struct avl_tree{
 void avl_tree_initialize(struct avl_tree *t, int (*cmp)(const void *, const void *), int offset);
 void *avl_tree_find(struct avl_tree *t, const void *data);
 void avl_tree_insert(struct avl_tree *t, void *data);
-//...
+struct avl_node *fix_balance(struct avl_node *root);
+void *avl_tree_find(struct avl_tree *t, const void *data);
+void avl_tree_insert(struct avl_tree *t, void *data);
+struct avl_node avl_tree_insert_rec(struct avl_tree *t, void *data, struct avl_node *n);
--- Students/ISEN/tp4/src/avl_trees.c
+++ Students/ISEN/tp4/src/avl_trees.c
@@ -1,6 +1,7 @@
 #include "avl_trees.h"
 //#include <stdlib.h>
 #include <string.h> //NULL, ...
+#include <stdbool.h>
 
 
 void avl_tree_initialize(struct avl_tree *t, int (*cmp)(const void *, const void *), int offset){
@@ -9,6 +10,94 @@ void avl_tree_initialize(struct avl_tree *t, int (*cmp)(const void *, const void
     t->offset = offset;
 }
 
+static int balance_factor(struct avl_node *root){
+    //copy from course_notes
+    if (!root){
+        return 0;
+    }
+    //height of left child and right child
+    int left = root->left ? root->left->height : 0;
+    int right = root->right ? root->right->height : 0;
+    return right - left;
+}
+
+static void uptdate_height(struct avl_node *root){
+    //also from course_notes
+    if (!root){
+        return;
+    }
+    int left = root->left ? root->left->height : 0;
+    int right = root->right ? root->right->height :0;
+    root->height = 1 + (left > right ? left : right);
+}
+
+static struct avl_node *rotate_right(struct avl_node *root){
+    //th child you want to rotate
+    struct avl_node *tmp_child = root;
+    //the child of the new root that will be the new left child of the old root
+    struct avl_node *tmp_childchild = root->left->right;
+
+    //new root
+    root = root->left;
+    //right = old root
+    root->right = tmp_child;
+    //left of the right --> former right of the left child
+    root->right->left = tmp_childchild;
+    update_height(root->right);
+    update_height(root);
+    return root;
+}
+
+static struct avl_node *rotate_left(struct avl_node *root){
+    struct avl_node *tmp_child = root;
+    struct avl_node *tmp_childchild = root->right->left;
+
+    root = root->right;
+    root->left = tmp_child;
+    root->left->right = tmp_childchild;
+    update_height(root->left);
+    update_height(root);
+    return root;
+}
+
+struct avl_node *fix_balance(struct avl_node *root){
+    //from course_notes & slightly adapted
+    int blnce = balance_factor(root);
+    if(abs(blnce) <= 1){  //no balancing needed
+        return root; 
+    }
+    //dir = heavy direction, not rotate direction
+    bool dir = blnce > 0;  
+    // dir = true --> balance > 0 --> right > left --> right heavy
+    // dir = false --> balance < 0 --> left heavy
+    
+    int bc;  //balance child 
+    if(dir){
+        //if its right heavy, we wand balance of right child
+        bc = balance_factor(root->right);
+    }else{
+        //if its left heaxy, we want balance of left child
+        bc = balance_factor(root->left);
+    }
+
+    bool dirc = bc > 0; 
+    //dir & dirc different --> first rotation in other direction
+    if (bc && (dir != dirc)){
+        if(dir){
+            root->right = rotate_right(root->right);
+        }else{
+            root->left = rotate_left(root->left);
+        }
+    }
+    if(dir){
+        root = rotate_left(root);
+    }else{
+        root = rotate_right(root);
+    }
+}
+
+
+//____________find_____________________________________________________
 
 void *avl_tree_find(struct avl_tree *t, const void *data){
     if(t->root == NULL){  //tree empty
@@ -36,6 +125,7 @@ void *avl_tree_find(struct avl_tree *t, const void *data){
     return NULL;
 }
 
+//_____________________insert___________________________________________
 void avl_tree_insert(struct avl_tree *t, void *data){
    *t->root = avl_tree_insert_rec(t, data, t->root);
 }
@@ -53,52 +143,15 @@ struct avl_node avl_tree_insert_rec(struct avl_tree *t, void *data, struct avl_n
     void *curr_data = &n - t->offset;
     int comp = t->cmp(data, curr_data);  //data of node and data to insert
     if(comp == 0){
-        return n; //--> already exists
-    } else if(comp < 0){
-        n->left = avl_tree_insert_rec(t, data, n->left);
-    } else {
-        n->right = avl_tree_insert_rec(t, data, n->right);
+        return *n; //--> already exists
+    }else if(comp < 0){
+        *n->left = avl_tree_insert_rec(t, data, n->left);
+    }else{
+        *n->right = avl_tree_insert_rec(t, data, n->right);
     }
     update_height(n);
     fix_balance(n);
 }
 
 
-static int balance_factor(struct avl_node *root){
-    //copy from course_notes
-    if (!root){
-        return 0;
-    }
-    //height of left child and right child
-    int left = root->left ? root->left->height : 0;
-    int right = root->right ? root->right->height : 0;
-    return right - left;
-}
 
-static void uptdate_height(struct avl_node *root){
-    //also from course_notes
-    if (!root){
-        return;
-    }
-    int left = root->left ? root->left->height : 0;
-    int right = root->right ? root->right->height :0;
-    root->height = 1 + (left > right ? left : right);
-}
-
-
-struct avl_node *fix_balance(struct avl_node *root){
-    int b = get_balance(root);
-    if(abs(b) <= 1){  //no balancing needed
-        return root; 
-    }
-    //....
-}
-
-
-
-static void left_rotate(){
-
-}
-static void right_rotate(){
-
-}
